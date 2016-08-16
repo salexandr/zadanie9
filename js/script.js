eea7a75cfd9950247f64e633a0f79220a13bdd35
Alexandr <a.senia@yandex.ru>
2016-08-16 15:08:17 +0400

    gotovoe

---
+++ js/script.js
@@ -0,0 +1,6 @@
+﻿      document.createElement('header');
+      document.createElement('nav');
+      document.createElement('section');
+      document.createElement('article');
+      document.createElement('aside');
+      document.createElement('footer');
